1d2a37df9ebad4c134d5ce273c2118eeb6f338ae
Netto17 <>
2023-08-25 20:01:59 -0300

    calcula a soma e a subtração de dois números quaisquer

--- calc.py
+++ calc.py
@@ -3,3 +3,5 @@ n1=float(input("Digite um número: "))
 n2=float(input("Digite outro número: "))
 soma=n1+n2
 print("A soma dos valores é: ",soma)
+subtracao=n1-n2
+print("A diferença entre os valores é: ",subtracao)
