88f6fb0dafbc343ee18f5b4f7dbb5e4cea56edaa
lab135 <>
2023-08-25 22:13:29 -0300

    calcula a soma, subtração,multiplicação e a divisão de dois números impedindo a divisão por zero

--- calc.py
+++ calc.py
@@ -7,3 +7,9 @@ subtracao=n1-n2
 print("A diferença entre os valores é: ",subtracao)
 mult=n1*n2
 print("A multiplicação dos valores é: ",mult)
+if n2!=0:
+    div=n1/n2
+    print("A divisão dos valores é: ",div)
+else:
+    print("Impissível dividir por 0!")
+    
